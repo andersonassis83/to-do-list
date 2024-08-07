49e66cd85a429c4f3bef9c426c3c4e8bfaff0a94
andersonassis83 <andersonassisartes@gmail.com>
2024-08-07 14:46:42 -0300

    commit components n´their styles

---
+++ src/app/modules/to-do-list/pages/list/list.component.ts
@@ -0,0 +1,36 @@
+import { Component, signal } from '@angular/core';
+
+//Components
+import { InputAddItemComponent } from '../../components/input-add-item/input-add-item.component';
+import { IListItems } from '../../interface/IListItems.interface';
+import { InputListItemComponent } from '../../components/input-list-item/input-list-item.component';
+
+
+@Component({
+  selector: 'app-list',
+  standalone: true,
+  imports: [InputAddItemComponent, InputListItemComponent],
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.scss'
+})
+export class ListComponent {
+  public addItem = signal(true);
+
+  #setListItems = signal<IListItems[]>(this.#parseItems());
+  getListItems = this.#setListItems.asReadonly();
+
+  #parseItems(){
+    return JSON.parse(localStorage.getItem('@my-list') || '[]');
+  }
+  
+  public getInputAndAddItem(value: IListItems){
+    localStorage.setItem('@my-list', JSON.stringify([...this.#setListItems(), value]));
+
+    return this.#setListItems.set(this.#parseItems());
+  }
+
+  public deleteAllItems(){
+    localStorage.removeItem('@my-list');
+    return this.#setListItems.set(this.#parseItems());
+  }
+}
